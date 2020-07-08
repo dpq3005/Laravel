c676e3efc50d77d6db8082cb3e92701d38a08636
dpq3005 <41153934+dpq3005@users.noreply.github.com>
2020-07-08 15:32:26 +0700

    Seed

---
+++ blog/database/seeds/ProductTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProductTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('products')->insert([
+            'name'=>Str::random(10),
+            'category'=>Str::random(10)
+        ]);
+    }
+}
